dc5e69dec79cabe7eafb69f68df21643dd69dfee
DanielBarragan96 <ie702554@iteso.mx>
2023-03-23 22:59:29 -0600

    added key value

--- src/Task.jsx
+++ src/Task.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./Task.css";
 
 export default function Task({ date, task }) {
-  return <div key={date + task.index}>{task.clienteMin + task.obraMin}</div>;
+  return <div className="task">{task.clienteMin + task.obraMin}</div>;
 }
