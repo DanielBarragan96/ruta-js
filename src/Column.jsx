db26ed8e099674326ccafbcb4389b0d66e3a7cc8
DanielBarragan96 <ie702554@iteso.mx>
2023-03-23 22:59:17 -0600

    removed id

--- src/Column.jsx
+++ src/Column.jsx
@@ -8,7 +8,7 @@ export default function Column({ date, tasks }) {
       <div className="task_list">
         <h2>{date}</h2>
         {tasks.map((task) => (
-          <Task date={date} task={task}></Task>
+          <Task key={date + task.index} date={date} task={task}></Task>
         ))}
       </div>
     </div>
