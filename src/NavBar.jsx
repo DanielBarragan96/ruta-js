af0bd9b5f7f3b457449e06be7033d4fbd2480758
DanielBarragan96 <ie702554@iteso.mx>
2023-06-15 16:27:54 -0600

    open and close modal

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import React from "react";
 import "./NavBar.css";
 
@@ -8,7 +9,6 @@ export default function NavBar({ date }) {
     date.substring(8, 10)
   );
   newDate.setMonth(newDate.getMonth() - 1);
-  console.log(newDate);
   const month = newDate
     .toLocaleString("ES-MX", { month: "short" })
     .toUpperCase();
@@ -17,15 +17,42 @@ export default function NavBar({ date }) {
     .toString()
     .substring(2, 4);
 
-  let addTask = () => {
-    console.log("press");
+  let [displayModal, setDisplayModal] = useState(false);
+  let showModal = () => {
+    setDisplayModal(!displayModal);
   };
+  let closeModal = () => {
+    setDisplayModal(false);
+  };
+
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.keyCode === 27) {
+        setDisplayModal(false);
+      }
+    };
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
 
   return (
     <div className="navbar_container">
       <button disabled></button>
       <button className="date">{month + year}</button>
-      <button onClick={addTask}>Add</button>
+      <button onClick={showModal}>Add</button>
+      <div
+        id="myModal"
+        className="modal"
+        style={{ display: displayModal ? "block" : "none" }}
+      >
+        <div className="modal-content">
+          <p>Some text in the Modal..</p>
+          <button onClick={closeModal}>Close</button>
+        </div>
+      </div>
     </div>
   );
 }
