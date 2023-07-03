cd8addbed178fc1825f08acba51eb991d518ca69
DanielBarragan96 <ie702554@iteso.mx>
2023-07-02 19:20:38 -0600

    fixed column titles

--- src/Column.jsx
+++ src/Column.jsx
@@ -3,11 +3,25 @@ import "./Column.css";
 import Task from "./Task";
 import { Droppable } from "react-beautiful-dnd";
 
+function getColumnTitle(date) {
+  let newDate = new Date(date);
+  let title = "";
+  title += newDate.getDate().toString();
+  title += Intl.DateTimeFormat("es", { month: "short" })
+    .format(newDate)
+    .toUpperCase();
+  title += newDate
+    .getFullYear()
+    .toString()
+    .substring(2);
+  return title;
+}
+
 export default function Column({ date, tasks, index }) {
   return (
     <div className="column_container">
       <div className="task_list">
-        <h2>{date}</h2>
+        <div className="title">{getColumnTitle(date)}</div>
         <Droppable droppableId={index.toString()}>
           {(provided) => (
             <div ref={provided.innerRef} {...provided.droppableProps}>
