b647e3b81fc32a2b639a4c22571416c0df2eb0f3
DanielBarragan96 <ie702554@iteso.mx>
2023-07-15 19:24:01 -0600

    simple inputs

--- src/CreateNewTask.jsx
+++ src/CreateNewTask.jsx
@@ -30,6 +30,14 @@ export default function ModalCreateNewTask({
   ReactModal.setAppElement("#root");
   let maxYear = currDate.getFullYear() + 1;
 
+  let [clienteMin, setClienteMin] = useState(task.clienteMin);
+  let [obraMin, setObraMin] = useState(task.obraMin);
+  let [equipo, setEquipo] = useState(task.equipo);
+  let [type, setType] = useState(task.type);
+  let [bandera, setBandera] = useState(task.type);
+  let [id, setId] = useState(task.id);
+  let [index, setIndex] = useState(task.index);
+
   return (
     <ReactModal
       isOpen={showModal}
@@ -55,7 +63,100 @@ export default function ModalCreateNewTask({
             value={taskDate}
           />
         </div>
-
+        <div>
+          <label htmlFor="cliente">Cliente:</label>
+          <input
+            type="text"
+            id="cliente"
+            placeholder="Cliente"
+            value={clienteMin}
+            onChange={(e) => {
+              task.clienteMin = e.target.value;
+              setClienteMin(e.target.value);
+            }}
+          />
+          <label htmlFor="obra">Obra:</label>
+          <input
+            type="text"
+            id="obra"
+            placeholder="Obra"
+            value={obraMin}
+            onChange={(e) => {
+              task.obraMin = e.target.value;
+              setObraMin(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="ES">ES:</label>
+          <input
+            type="text"
+            id="ES"
+            placeholder="ES"
+            value={type}
+            onChange={(e) => {
+              task.type = e.target.value;
+              setType(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="equipo">Equipo:</label>
+          <input
+            type="text"
+            id="equipo"
+            placeholder="Equipo"
+            value={equipo}
+            onChange={(e) => {
+              task.equipo = e.target.value;
+              setEquipo(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="bandera">Bandera:</label>
+          <input
+            type="text"
+            id="bandera"
+            placeholder="Bandera"
+            value={bandera}
+            onChange={(e) => {
+              task.bandera = e.target.value;
+              setBandera(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="index">Index:</label>
+          <input
+            type="int"
+            id="index"
+            placeholder="Index"
+            value={index}
+            onChange={(e) => {
+              task.index = e.target.value;
+              setIndex(e.target.value);
+            }}
+          />
+          <label htmlFor="id">Id:</label>
+          <input
+            type="text"
+            id="id"
+            placeholder="Id"
+            value={id}
+            onChange={(e) => {
+              task.id = e.target.value;
+              setId(e.target.value);
+            }}
+          />
+        </div>
+        <button
+          onClick={() => {
+            console.log(task);
+          }}
+        >
+          Results
+        </button>
         <button onClick={handleCloseModal}>Cancelar</button>
       </div>
     </ReactModal>
