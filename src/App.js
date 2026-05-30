48d2f9bddd7d891a5c75a6ca7ff3c5440c258099
DanielBarragan96 <ie702554@iteso.mx>
2026-05-29 22:11:53 -0600

    fix: date bug after modal edit

--- src/App.js
+++ src/App.js
@@ -219,12 +219,15 @@ function App() {
   let insertTask = (task) => {
     let currDateIndex = currWeek.indexOf(task.date);
     if (currDateIndex === -1) return;
-    let existing = data[currDateIndex].find((el) => el.id === task.id);
-    if (existing !== undefined) {
-      data[currDateIndex][existing.index] = task;
-    } else {
-      data[currDateIndex].splice(task.index, 0, task);
+    // Remove from whichever column currently holds this id (date may have changed)
+    for (let day = 0; day < data.length; day++) {
+      const idx = data[day].findIndex((el) => el.id === task.id);
+      if (idx !== -1) {
+        data[day].splice(idx, 1);
+        break;
+      }
     }
+    data[currDateIndex].splice(task.index, 0, task);
     restartIndexes();
     saveTasks(data.flat());
     setData([...data]);
